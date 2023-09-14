b0a94c59a5075a8bd87fa31b6d35b9af99df1fe9
Andrej <75538726+AndrejBuraz@users.noreply.github.com>
2023-09-14 19:51:11 +0200

    Add files via upload

---
+++ InicijalnoPonavljanje2/InicijalnoPonavljanje2/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InicijalnoPonavljanje2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Unesi niz znakova: ");
+            string znakovi = Console.ReadLine();
+            Console.WriteLine("Niz u kojem su razmaci zamijenjeni sa _: " + znakovi.Replace(" ", "_"));
+            Console.ReadKey();
+        }
+    }
+}
